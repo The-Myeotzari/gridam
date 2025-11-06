8a72dcb34a5cdb99673de1c4c7d8e99493aeb7e5
cji <gugugeul273@gmail.com>
2025-11-06 09:44:35 +0900

    feat(input):코멘트 반영하여 수정

--- gridam/src/components/common/input.tsx
+++ gridam/src/components/common/input.tsx
@@ -1,21 +1,34 @@
 'use client'
 import * as React from 'react'
-
 import cn from '@/utils/cn'
 
-const Input = React.forwardRef<HTMLInputElement, React.ComponentProps<'input'>>(
-  ({ type, className, ...props }, ref) => {
+type SizeStyle = 'default' | 'sm' | 'md' | 'lg'
+const SIZE_STYLE: Record<SizeStyle, string> = {
+  default: 'h-10 w-72',
+  sm: 'h-4 w-32',
+  md: 'h-6 w-48',
+  lg: 'h-8 w-64',
+}
+
+interface InputType extends React.ComponentProps<'input'> {
+  sizeStyle?: SizeStyle
+}
+
+const Input = React.forwardRef<HTMLInputElement, InputType>(
+  ({ sizeStyle = 'default', type = 'text', className = 'string', ...props }, ref) => {
     return (
       <input
         type={type}
         className={cn(
-          'flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-base ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 md:text-sm',
+          'flex rounded-md border border-input bg-background px-3 py-2 text-base ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 md:text-sm',
+          SIZE_STYLE[sizeStyle],
           className
         )}
-        {...props}
         ref={ref}
+        {...props}
       ></input>
     )
   }
 )
+
 export default Input
