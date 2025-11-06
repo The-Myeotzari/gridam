464ec1f893b7bbaecd127430b46b505301f847eb
OlMinJe <Ol.MinJe@gmail.com>
2025-11-06 20:19:49 +0900

    🔧 file: supabase 테스트 코드 삭제

--- gridam/src/app/(main)/ssrcountries/country.tsx
+++ gridam/src/app/(main)/ssrcountries/country.tsx
@@ -1,18 +1,16 @@
 'use client'
 
-import { getCountryById } from '@/queries/get-country-by-id'
-import useSupabaseBrowser from '@/utils/supabase/client'
-import { useQuery } from '@supabase-cache-helpers/postgrest-react-query'
+// import useSupabaseBrowser from '@/utils/supabase/client'
 
 export default function Country({ id }: { id: number }) {
-  const supabase = useSupabaseBrowser()
+  // const supabase = useSupabaseBrowser()
   // This useQuery could just as well happen in some deeper
   // child to <Posts>, data will be available immediately either way
-  const { data: country } = useQuery(getCountryById(supabase, id))
+  // const { data: country } = useQuery(getCountryById(supabase, id))
 
   return (
     <div>
-      <h1>SSR: {country?.name}</h1>
+      <h1>SSR: </h1>
     </div>
   )
 }
