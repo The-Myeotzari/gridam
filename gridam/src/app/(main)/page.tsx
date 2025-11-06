a5712d2256658761b0156f832faa715e262f1466
OlMinJe <Ol.MinJe@gmail.com>
2025-11-06 15:59:27 +0900

    fix: 메인 변경 되돌림

--- gridam/src/app/(main)/page.tsx
+++ gridam/src/app/(main)/page.tsx
@@ -1,10 +1,3 @@
-import CanvasContainer from '@/components/common/canvas-container'
-
 export default function Page() {
-  return (
-    <>
-      메인 페이지 입니다.
-      <CanvasContainer />
-    </>
-  )
+  return <>메인 페이지 입니다.</>
 }
