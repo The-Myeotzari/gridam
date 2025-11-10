f3dad2d9a6427a74af3bad6e9830c32f3dfca027
cji <gugugeul273@gmail.com>
2025-11-10 09:47:55 +0900

    feat(MSG): 이메일 유효성 검사 항목 추가

--- gridam/src/constants/messages.ts
+++ gridam/src/constants/messages.ts
@@ -18,6 +18,7 @@ export const MESSAGES = {
       EMPTY_FORM: '모든 항목을 입력해주세요.',
       INVALID_PASSWORD_LENGTH: '비밀번호는 8자 이상이어야 합니다.',
       INVALID_PASSWORD_FORMAT: '비밀번호에는 대소문자와 특수문자가 포함되어야 합니다.',
+      INVALID_EMAIL_FORMAT: '이메일 형식이 올바르지 않습니다.',
     },
   },
 
