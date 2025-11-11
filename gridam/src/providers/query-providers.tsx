8c94dc7be8e4ab3c8206c3ac7e79cca77ba5e0ed
OlMinJe <Ol.MinJe@gmail.com>
2025-11-11 19:41:00 +0900

    fix: provider 서버 컴포넌트 생성하여 래퍼 역할하도록 분리 #84

--- gridam/src/providers/query-providers.tsx
+++ gridam/src/providers/query-providers.tsx
@@ -1,8 +1,6 @@
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { ReactNode } from 'react'
+import { ClientQueryProvider } from '@/providers/query-providers.client'
+import type { ReactNode } from 'react'
 
-const queryClient = new QueryClient()
-
-export default function RootLayout({ children }: { children: ReactNode }) {
-  return <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+export default function QueryProvider({ children }: { children: ReactNode }) {
+  return <ClientQueryProvider>{children}</ClientQueryProvider>
 }
