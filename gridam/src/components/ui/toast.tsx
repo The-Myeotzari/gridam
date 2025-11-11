8823b6b773a639a82cc01a14a9995a66560bd54b
tjdwns99 <beans2853@gmail.com>
2025-11-11 16:00:35 +0900

    💄 refactor: 토스트 div로 변경

--- gridam/src/components/ui/toast.tsx
+++ gridam/src/components/ui/toast.tsx
@@ -1,9 +1,9 @@
 'use client'
 
 import { useToast } from '@/store/toast-store'
+import cn from '@/utils/cn'
 import type { Toast } from '@/utils/type'
 import { AlertCircleIcon, CheckCircle2Icon } from 'lucide-react'
-import { Card, CardBody } from './card'
 
 export default function Toast() {
   const { items } = useToast()
@@ -13,17 +13,23 @@ export default function Toast() {
     <div className="fixed bottom-6 right-1 md:right-6 z-9999 space-y-3">
       {items.map((t: Toast) => (
         // 개별 토스트 카드
-        <Card key={t.id} className="bg-cream-white">
-          <CardBody className="flex items-center gap-2">
-            {t.type === 'success' ? (
-              <CheckCircle2Icon className="size-6 text-white fill-black" />
-            ) : (
-              <AlertCircleIcon className="size-6 text-white fill-black" />
-            )}
-            {/* 메시지 텍스트 */}
-            <span className="text-sm">{t.message}</span>
-          </CardBody>
-        </Card>
+        <div
+          key={t.id}
+          className={cn(
+            'rounded-lg border border-border bg-cream-white text-card-foreground shadow-sm',
+            'px-5 py-4',
+            'flex items-center gap-2'
+          )}
+        >
+          {t.type === 'success'
+            ?
+            <CheckCircle2Icon className='size-6 text-white fill-black' />
+            :
+            <AlertCircleIcon className='size-6 text-white fill-black' />
+          }
+          {/* 메시지 텍스트 */}
+          <span className="text-sm">{t.message}</span>
+        </div>
       ))}
     </div>
   )
