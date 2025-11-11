d2379a2318861ccd3c9f23b4427e1b0e93d2cb0f
OlMinJe <Ol.MinJe@gmail.com>
2025-11-11 15:05:22 +0900

    reafactor: 버튼 컴포넌트 서버 컴포넌트로 변경 #84

--- gridam/src/test/components/ui/button.test.tsx
+++ gridam/src/test/components/ui/button.test.tsx
@@ -1,4 +1,4 @@
-import Button from '@/components/ui/button.client'
+import Button from '@/components/ui/button'
 import '@testing-library/jest-dom'
 import { fireEvent, render, screen } from '@testing-library/react'
 
