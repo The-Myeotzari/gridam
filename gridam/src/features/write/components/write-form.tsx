daafae2671119b9c79c5a9aec34bd93512327256
OlMinJe <Ol.MinJe@gmail.com>
2025-11-13 15:53:13 +0900

    ✨ feat: 글작성 취소 이벤트 구현 #118

--- gridam/src/features/write/components/write-form.tsx
+++ gridam/src/features/write/components/write-form.tsx
@@ -1,13 +1,16 @@
 'use client'
 
+import { ModalBody, ModalFooter, ModalHeader } from '@/components/ui/modal/modal'
 import Textarea from '@/components/ui/textarea'
+import { MESSAGES } from '@/constants/messages'
 import { usePostDiary } from '@/features/write/api/queries/use-post-diary'
 import CanvasContainer from '@/features/write/components/canvas-container'
-import WriteButtons from '@/features/write/components/write-buttons'
 import { useCanvasStore } from '@/features/write/store/canvas-store'
 import { useSetDate, useSetText, useWriteStore } from '@/features/write/store/write-store'
+import { modalStore } from '@/store/modal-store'
 import { useRouter } from 'next/navigation'
 import { useEffect } from 'react'
+import WriteButton from './write-button'
 
 type props = {
   today: string
@@ -47,6 +50,29 @@ export default function WriteForm({ today, dateValue, weather }: props) {
     })
   }
 
+  const handleCancel = () => {
+    modalStore.open((close) => (
+      <>
+        <ModalHeader>{MESSAGES.DIARY.CANCEL.TITLE}</ModalHeader>
+        <ModalBody className="p-6 text-slate-600">{MESSAGES.DIARY.CANCEL.DESCRIPTION}</ModalBody>
+        <ModalFooter className="p-4 flex justify-end gap-2">
+          <button className="border px-3 py-2 rounded" onClick={close}>
+            {MESSAGES.COMMON.CANCEL}
+          </button>
+          <button
+            className="bg-black text-white px-3 py-2 rounded"
+            onClick={() => {
+              router.back()
+              close()
+            }}
+          >
+            {MESSAGES.COMMON.CONFIRM}
+          </button>
+        </ModalFooter>
+      </>
+    ))
+  }
+
   return (
     <form onSubmit={handleSubmit}>
       <CanvasContainer />
@@ -55,7 +81,18 @@ export default function WriteForm({ today, dateValue, weather }: props) {
         <Textarea onChange={(v) => setText(v)} />
       </section>
 
-      <WriteButtons />
+      <div className="text-center mb-4">
+        <span onClick={handleCancel} className="mr-2">
+          <WriteButton label={MESSAGES.COMMON.CANCEL_BUTTON} type="button" />
+        </span>
+        <WriteButton
+          label={MESSAGES.COMMON.SAVE_BUTTON}
+          type="submit"
+          variant="blue"
+          isPending={isPending}
+          className="ml-2"
+        />
+      </div>
     </form>
   )
 }
