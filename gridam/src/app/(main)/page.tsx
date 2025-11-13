7387576e7c3e87c04a0c49000977313a247f8f6b
OlMinJe <Ol.MinJe@gmail.com>
2025-11-13 21:23:30 +0900

    💄 UI: 메인 페이지 UI 구현 #124

--- gridam/src/app/(main)/page.tsx
+++ gridam/src/app/(main)/page.tsx
@@ -1,11 +1,36 @@
 import Button from '@/components/ui/button'
+import FeedCard from '@/features/feed/components/feed-card'
+import Month from '@/features/feed/components/month'
 import { Plus } from 'lucide-react'
 import Link from 'next/link'
 
-export default function Home() {
+type props = {
+  searchParams?: {
+    [key: string]: string | string[] | undefined
+    year?: string
+    month?: string
+  }
+}
+
+export default async function Home({ searchParams }: props) {
+  const params = await searchParams // 반드시 await 필요!
+
+  const year = typeof params?.year === 'string' ? params.year : '2000'
+  const month = typeof params?.month === 'string' ? params.month : '02'
+
   return (
     <div className="flex flex-col gap-4 p-4 mt-10 text-center">
-      메인페이지 입니다.
+      <div className="mb-8 text-center animate-fade-in">
+        <h1 className="font-handwritten text-4xl mb-2 text-navy-gray">오늘의 이야기들</h1>
+        <p className="font-handwritten text-xl text-muted-foreground">
+          모두의 하루를 담은 그림 일기
+        </p>
+      </div>
+
+      <Month year={year} month={month} />
+
+      <FeedCard />
+
       <Link href="/write">
         <Button
           size="lg"
