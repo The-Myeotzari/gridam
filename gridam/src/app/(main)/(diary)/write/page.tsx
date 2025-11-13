4557c4adef782bcf94e08127adab3ee792608a5a
OlMinJe <Ol.MinJe@gmail.com>
2025-11-13 14:15:54 +0900

    🎨 refactor: 날씨 컴포넌트 페이지에 병합 #118

--- gridam/src/app/(main)/(diary)/write/page.tsx
+++ gridam/src/app/(main)/(diary)/write/page.tsx
@@ -1,4 +1,5 @@
-import WeatherContainer from '@/features/write/components/weather/weather-container'
+import { fetchWeather } from '@/features/write/api/weather.api'
+import WeatherIcon, { mapWeatherIdToIcon } from '@/features/write/components/weathre-icon'
 import WriteForm from '@/features/write/components/write-form'
 import { formatDate } from '@/utils/format-date'
 import { getCoordsFromCookies } from '@/utils/get-coords-from-cookies'
@@ -7,7 +8,9 @@ import { cookies } from 'next/headers'
 export default async function Page() {
   const cookie = await cookies()
   const coords = getCoordsFromCookies(cookie)
+  const data = await fetchWeather(coords.lat, coords.lon)
   const today = formatDate()
+  const wearther = mapWeatherIdToIcon(data.id)
 
   return (
     <div
@@ -19,9 +22,15 @@ export default async function Page() {
         style={{ borderColor: 'black' }}
       >
         <div className="font-bold text-md sm:text-xl">{today}</div>
-        <WeatherContainer lat={coords.lat} lon={coords.lon} />
+
+        <section className="flex items-center leading-9 gap-2">
+          <div className="font-bold text-md hidden sm:block">날씨</div>
+          <WeatherIcon id={data.id} icon={data.icon} alt={data.description} size={36} />
+          {/* <div className="text-md opacity-80">{Math.round(data.temp)}°C</div> */}
+        </section>
       </section>
-      <WriteForm today={today} />
+
+      <WriteForm today={today} weather={wearther} />
     </div>
   )
 }
