577b4df31d2e008bd8d56f68157e8bd7f03ccebe
cji <gugugeul273@gmail.com>
2025-11-14 03:29:19 +0900

    feat(REG):<RegisterInput>컴포넌트 생성

--- gridam/src/features/auth/register/components/register-input.tsx
+++ gridam/src/features/auth/register/components/register-input.tsx
@@ -1,20 +1,41 @@
-export default function registerInput() {
+import Input from '@/components/ui/input'
+import { FieldValues, RegisterOptions, UseFormRegister, Path } from 'react-hook-form'
+
+interface RegisterInputProps<TFormValues extends FieldValues> {
+  label: string
+  type?: string
+  name: Path<TFormValues>
+  placeholder?: string
+  register: UseFormRegister<TFormValues>
+  validation?: RegisterOptions<TFormValues>
+  className?: string
+  required?: boolean
+}
+
+export default function RegisterInput<TFormValues extends FieldValues>({
+  label,
+  type,
+  name,
+  register,
+  placeholder,
+  validation,
+  className,
+  required,
+}: RegisterInputProps<TFormValues>) {
   return (
     <>
-      <Input
-        type="text"
-        {...register('nickname', {
-          validate: (value) => {
-            if (!NICKNAME_REGEX.test(value)) {
-              return MESSAGES.AUTH.ERROR.INVALID_NICKNAME_FORMAT
-            }
-          },
-          required: true,
-        })}
-        id="nickname"
-        className="w-full"
-        placeholder="귀여운 닉네임"
-      />
+      <div className="flex flex-col gap-2 ">
+        <label htmlFor={name as string} className="text-lg text-left font-semibold">
+          {label}
+        </label>
+        <Input
+          type={type}
+          {...register(name, validation)}
+          id={String(name)}
+          placeholder={placeholder}
+          className={className || 'w-full'}
+        />
+      </div>
     </>
   )
 }
