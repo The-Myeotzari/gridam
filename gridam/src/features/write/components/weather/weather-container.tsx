fb7f148bf60fe6b838503cedb0fe88ab14a7d063
OlMinJe <Ol.MinJe@gmail.com>
2025-11-13 11:28:28 +0900

    💄 UI: 날씨 헤더 구성 수정 #114

--- gridam/src/features/write/components/weather/weather-container.tsx
+++ gridam/src/features/write/components/weather/weather-container.tsx
@@ -7,10 +7,10 @@ export default async function WeatherContainer({ lat, lon }: Props) {
   const data = await fetchWeather(lat, lon)
 
   return (
-    <section className="flex items-center leading-9 gap-1">
+    <section className="flex items-center leading-9 gap-2">
       <div className="font-bold text-md hidden sm:block">날씨</div>
       <WeatherIcon id={data.id} icon={data.icon} alt={data.description} size={36} />
-      <div className="text-md opacity-80">{Math.round(data.temp)}°C</div>
+      {/* <div className="text-md opacity-80">{Math.round(data.temp)}°C</div> */}
     </section>
   )
 }
