1a3bac35baf0bbba309b170f619782db172612e2
cji <gugugeul273@gmail.com>
2025-11-15 16:11:09 +0900

    feat(REG): 오류메시지 수정

--- gridam/src/features/auth/register/api/register-actions.ts
+++ gridam/src/features/auth/register/api/register-actions.ts
@@ -12,7 +12,7 @@ export async function registerUser(
   } catch (error: unknown) {
     if (error instanceof AxiosError) {
       if (error.response?.status === 409) {
-        throw new Error('이미 가입된 계정입니다. 이메일 인증을 진행해주세요.')
+        throw new Error(error.response?.data?.message)
       }
 
       throw new Error(error.response?.data?.message || MESSAGES.AUTH.ERROR.REGISTER)
