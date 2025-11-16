6d49104858e34ff87c00b9abe781c75549ccc3d7
tjdwns99 <beans2853@gmail.com>
2025-11-16 16:19:31 +0900

    ✨feat: 로그아웃 기능 구현

---
+++ gridam/src/features/mypage/api/queries/use-logout.ts
@@ -0,0 +1,16 @@
+'use client'
+
+import { api } from '@/lib/api'
+import { useMutation, useQueryClient } from '@tanstack/react-query'
+
+export function useLogout() {
+  const queryClient = useQueryClient()
+
+  return useMutation({
+    mutationFn: () => api.post('/auth/logout'),
+    onSuccess: () => {
+      // 유저 관련 캐시 싹 비우고 싶으면
+      queryClient.clear()
+    }
+  })
+}
