d6687bec4a84e56019c2c6877d804edab68e19a0
OlMinJe <Ol.MinJe@gmail.com>
2025-11-17 14:32:39 +0900

    ✅ refactor: 버튼 disabled 속성 추가 #106

--- gridam/src/features/feed/components/feed-list-error.tsx
+++ gridam/src/features/feed/components/feed-list-error.tsx
@@ -13,10 +13,7 @@ export default function FeedListError({ onRetry, isLoading }: Props) {
     <div className="flex flex-col items-center gap-2 text-lg">
       <p>{MESSAGES.DIARY.ERROR.READ}</p>
       <span onClick={onRetry}>
-        <Button
-          label={isLoading ? '다시 시도 중...' : '다시 시도'}
-          //  disabled={isLoading}
-        />
+        <Button label={isLoading ? '다시 시도 중...' : '다시 시도'} disabled={isLoading} />
       </span>
     </div>
   )
