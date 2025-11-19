013025ca0a3263c7ed7375eace6cd7c6e421d11f
OlMinJe <Ol.MinJe@gmail.com>
2025-11-19 14:12:53 +0900

    ♻️ refactor: 버튼 이벤트 개선으로 인한 버튼 컴포넌트 수정 #149

--- gridam/src/features/diary-detail/components/buttons/diary-save-button.tsx
+++ gridam/src/features/diary-detail/components/buttons/diary-save-button.tsx
@@ -2,18 +2,22 @@ import { MESSAGES } from '@/constants/messages'
 import DiaryFormButton from '@/features/diary-detail/components/diary-form-button'
 
 type props = {
-  createPending: boolean
-  uploadPending: boolean
+  isPending: boolean
+  onClick: () => void
 }
 
-export default function DiarySaveButton({ createPending, uploadPending }: props) {
+// 임시 저장 버튼 여기에 추가할까....
+// 임시 수정의 경우도 여기인거 같은데..
+export default function DiarySaveButton({ isPending, onClick }: props) {
   return (
-    <DiaryFormButton
-      label={MESSAGES.COMMON.SAVE_BUTTON}
-      type="submit"
-      variant="blue"
-      isPending={createPending || uploadPending}
-      className="ml-2"
-    />
+    <span onClick={onClick}>
+      <DiaryFormButton
+        label={MESSAGES.COMMON.SAVE_BUTTON}
+        type="submit"
+        variant="blue"
+        isPending={isPending}
+        className="ml-2"
+      />
+    </span>
   )
 }
--- gridam/src/features/diary-detail/components/buttons/diary-update-button.tsx
+++ gridam/src/features/diary-detail/components/buttons/diary-update-button.tsx
@@ -2,18 +2,20 @@ import { MESSAGES } from '@/constants/messages'
 import DiaryFormButton from '@/features/diary-detail/components/diary-form-button'
 
 type props = {
-  updatePending: boolean
-  uploadPending: boolean
+  isPending: boolean
+  onClick: () => void
 }
 
-export default function DiaryUpdateButton({ updatePending, uploadPending }: props) {
+export default function DiaryUpdateButton({ isPending, onClick }: props) {
   return (
-    <DiaryFormButton
-      label={MESSAGES.COMMON.UPDATE_BUTTON}
-      type="submit"
-      variant="blue"
-      isPending={updatePending || uploadPending}
-      className="ml-2"
-    />
+    <span onClick={onClick}>
+      <DiaryFormButton
+        label={MESSAGES.COMMON.UPDATE_BUTTON}
+        type="submit"
+        variant="blue"
+        isPending={isPending}
+        className="ml-2"
+      />
+    </span>
   )
 }
