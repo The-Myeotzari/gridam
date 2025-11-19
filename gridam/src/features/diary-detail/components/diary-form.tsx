3cddd9b6b140affb58ebd4cd09faccba44dde7ef
OlMinJe <Ol.MinJe@gmail.com>
2025-11-19 12:42:48 +0900

    ♻️ refactor: 다이어리 저장 취소 버튼 및 이벤트 컴포넌트화 #149

--- gridam/src/features/diary-detail/components/diary-form.tsx
+++ gridam/src/features/diary-detail/components/diary-form.tsx
@@ -1,7 +1,6 @@
 'use client'
 // 어떻게 최소한의 클라이언트로 만들지
 
-import { ModalBody, ModalFooter, ModalHeader } from '@/components/ui/modal/modal'
 import Textarea from '@/components/ui/textarea'
 import { MESSAGES } from '@/constants/messages'
 import { postDiaryAction } from '@/features/diary-detail/api/action/post-diary-action'
@@ -11,9 +10,8 @@ import { useUpdateDiary } from '@/features/diary-detail/api/queries/use-update-d
 import CanvasContainer from '@/features/diary-detail/components/canvas/canvas-container'
 import DiaryFormButton from '@/features/diary-detail/components/diary-form-button'
 import { useDiaryForm } from '@/features/diary-detail/hooks/use-diary-form'
-import { modalStore } from '@/store/modal-store'
-import { useRouter } from 'next/navigation'
-import { useCallback, useEffect, useState } from 'react'
+import { useEffect, useState } from 'react'
+import DiaryCancelButton from './buttons/diary-cancel-button'
 
 type props = {
   dateValue: string
@@ -32,8 +30,6 @@ export default function DiaryForm({
   initialContent,
   initialImage,
 }: props) {
-  const router = useRouter()
-
   const { date, text, setText, setDate } = useDiaryForm()
   const [canvas, setCanvas] = useState<string | null>(initialImage ?? null)
 
@@ -72,29 +68,6 @@ export default function DiaryForm({
     })
   }
 
-  const handleCancel = useCallback(() => {
-    modalStore.open((close) => (
-      <>
-        <ModalHeader>{MESSAGES.DIARY.CANCEL.TITLE}</ModalHeader>
-        <ModalBody className="p-6 text-slate-600">{MESSAGES.DIARY.CANCEL.DESCRIPTION}</ModalBody>
-        <ModalFooter className="p-4 flex justify-end gap-2">
-          <button className="border px-3 py-2 rounded" onClick={close}>
-            {MESSAGES.COMMON.CANCEL}
-          </button>
-          <button
-            className="bg-black text-white px-3 py-2 rounded"
-            onClick={() => {
-              router.back()
-              close()
-            }}
-          >
-            {MESSAGES.COMMON.CONFIRM}
-          </button>
-        </ModalFooter>
-      </>
-    ))
-  }, [router])
-
   return (
     <form onSubmit={handleSubmit}>
       <CanvasContainer initialImage={initialImage} onChange={(img) => setCanvas(img)} />
@@ -104,9 +77,7 @@ export default function DiaryForm({
       </section>
 
       <div className="text-center mb-4">
-        <span onClick={handleCancel} className="mr-2">
-          <DiaryFormButton label={MESSAGES.COMMON.CANCEL_BUTTON} type="button" />
-        </span>
+        <DiaryCancelButton />
 
         <DiaryFormButton
           label={isEdit ? MESSAGES.COMMON.UPDATE_BUTTON : MESSAGES.COMMON.SAVE_BUTTON}
