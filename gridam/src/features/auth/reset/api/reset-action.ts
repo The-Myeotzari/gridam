aaaa2dd003c4be09580327748c476f0aaea613b5
cji <gugugeul273@gmail.com>
2025-11-19 16:36:57 +0900

    feat:비밀번호 재설정 action

--- gridam/src/features/auth/reset/api/reset-action.ts
+++ gridam/src/features/auth/reset/api/reset-action.ts
@@ -1,5 +1,34 @@
 'use server'
 
+import { MESSAGES } from '@/constants/messages'
+import { api } from '@/lib/api'
+import axios, { AxiosError } from 'axios'
+import { error } from 'console'
+
 export async function resetAction(formData: FormData) {
-  console.log(formData)
+  console.log('✅ resetAction 호출됨')
+  console.log('📦 formData 내용:', Object.fromEntries(formData.entries()))
+
+  const token = formData.get('token')
+  const password = formData.get('password')
+  const confirmPassword = formData.get('comfirmPassword')
+
+  try {
+    const response = await axios.post('apis/auth/reset/complete', {
+      token,
+      password,
+      confirmPassword,
+    })
+    console.log('응답', response)
+    return response.data
+  } catch (error) {
+    if (error instanceof AxiosError) {
+      console.error('error', error.response?.data || error)
+      return {
+        error:
+          error.response?.data?.message || MESSAGES.AUTH.ERROR.EMAIL_VERIFICATION_REQUEST_FAILED,
+      }
+    }
+  }
+  return { error: MESSAGES.AUTH.ERROR.PASSWORD_RESET }
 }
