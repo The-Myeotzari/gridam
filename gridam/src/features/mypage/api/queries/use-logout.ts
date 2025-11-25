51cc6c48ec853c0ee76102f8561997799568da1d
tjdwns99 <beans2853@gmail.com>
2025-11-25 11:34:07 +0900

    ♻️ refactor: 로그아웃 코드 수정

--- gridam/src/features/mypage/api/queries/use-logout.ts
+++ gridam/src/features/mypage/api/queries/use-logout.ts
@@ -1,18 +1,31 @@
 'use client'
 
+import { MESSAGES } from '@/shared/constants/messages'
 import { QUERY_KEYS } from '@/shared/constants/query-key'
+import { toast } from '@/store/toast-store'
 import { useMutation, useQueryClient } from '@tanstack/react-query'
-import axios from 'axios'
+import axios, { AxiosError } from 'axios'
+import { useRouter } from 'next/navigation'
 
+// TODO: Server Actions으로 변경
 export function useLogout() {
   const queryClient = useQueryClient()
+  const router = useRouter()
 
   return useMutation({
     mutationKey: QUERY_KEYS.AUTH.LOGOUT,
     mutationFn: () => axios.post('/apis/auth/logout'),
     onSuccess: () => {
       // 유저 관련 캐시 비우고 싶으면
+      toast.success(MESSAGES.AUTH.SUCCESS.LOGOUT)
+      router.push('/login')
+      router.refresh()
       queryClient.clear()
+    },
+    onError: (err) => {
+      const message =
+        err instanceof AxiosError ? err.response?.data.message : MESSAGES.AUTH.ERROR.LOGOUT
+      toast.error(message)
     },
   })
 }
--- gridam/src/features/mypage/components/mypage-buttons.tsx
+++ gridam/src/features/mypage/components/mypage-buttons.tsx
@@ -2,32 +2,12 @@
 
 import { useLogout } from '@/features/mypage/api/queries/use-logout'
 import ChangePasswordModal from '@/features/mypage/components/change-password-modal'
-import { MESSAGES } from '@/shared/constants/messages'
 import Button from '@/shared/ui/button'
 import { modalStore } from '@/store/modal-store'
-import { toast } from '@/store/toast-store'
-import { AxiosError } from 'axios'
 import { Key, LogOut } from 'lucide-react'
-import { useRouter } from 'next/navigation'
 
 export default function MyPageButtons() {
-  const router = useRouter()
-  const { mutate, isPending } = useLogout()
-
-  const handleLogout = () => {
-    mutate(undefined, {
-      onSuccess: () => {
-        toast.success(MESSAGES.AUTH.SUCCESS.LOGOUT)
-        router.push('/login')
-        router.refresh()
-      },
-      onError: (err) => {
-        const message =
-          err instanceof AxiosError ? err.response?.data.message : MESSAGES.AUTH.ERROR.LOGOUT
-        toast.error(message)
-      },
-    })
-  }
+  const { mutate: handleLogout, isPending } = useLogout()
 
   return (
     <section className="flex gap-2">
@@ -43,7 +23,7 @@ export default function MyPageButtons() {
           variant="roundedBasic"
         />
       </span>
-      <span onClick={handleLogout} className={isPending ? 'pointer-events-none opacity-50' : ''}>
+      <span onClick={() => handleLogout()} className={isPending ? 'pointer-events-none opacity-50' : ''}>
         <Button
           type="button"
           label={
