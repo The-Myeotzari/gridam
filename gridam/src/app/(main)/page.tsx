a872c56595defff2ddad2e1e90eca8a1c954201c
OlMinJe <Ol.MinJe@gmail.com>
2025-11-26 18:47:58 +0900

    🐛 hotfix: 메인 페이지 예외처리 오류 수정

--- gridam/src/app/(main)/page.tsx
+++ gridam/src/app/(main)/page.tsx
@@ -16,31 +16,43 @@ export default async function Home({ searchParams }: PageProps) {
   const { year, month } = resolveYearMonth(params)
 
   const { ok, data: firstPage } = await fetchDiaryPage({ year, month, cursor: null })
+  
+  const renderContent = () => {
+    if (!ok && firstPage) {
+      return (
+        <div className="h-50 flex flex-col justify-center items-center">
+          <p className="mb-4">{MESSAGES.DIARY.ERROR.READ}</p>
+          <Link href="/">
+        <Button 
+          label={
+            <div className="flex items-center">
+              <RefreshCcw className="mr-2" />
+              <span>새로고침</span>
+            </div>
+          }
+        />
+        </Link>
+      </div>
+      )
+    }
+
+    if (!firstPage) {
+      return (
+      <div className="text-center py-16">
+        <p className="font-handwritten text-2xl text-muted-foreground mb-4">아직 일기가 없어요</p>
+        <p className="font-handwritten text-lg text-muted-foreground mb-8">첫 번째 일기를 작성해보세요!</p>
+        </div>
+      )
+    }
+    return <FeedList year={year} month={month} initialPage={firstPage} />
+  }
 
   return (
     <div className="flex flex-col gap-4 p-4 mt-10 text-center">
       <h1 className="font-bold text-4xl mb-2 text-navy-gray">오늘의 이야기들</h1>
       <p className="font-bold text-xl text-muted-foreground">모두의 하루를 담은 그림 일기</p>
-
-      {ok ? (
-        <FeedList year={year} month={month} initialPage={firstPage} />
-      ) : (
-        <div className="h-50 flex flex-col justify-center items-center">
-          <p className="mb-4">{MESSAGES.DIARY.ERROR.READ}</p>
-          <Link href="/">
-            <Button
-              label={
-                <div className="flex items-center">
-                  <RefreshCcw className="mr-2" />
-                  <span>새로고침</span>
-                </div>
-              }
-            />
-          </Link>
-        </div>
-      )}
-
-      <FeedWriteBtn todayDiaryStatus={firstPage?.todayDiaryStatus ? 'none' :firstPage?.todayDiaryStatus } />
-    </div>
+      {renderContent()}
+      <FeedWriteBtn todayDiaryStatus={firstPage?.todayDiaryStatus ? 'none' : firstPage?.todayDiaryStatus}/>
+  </div>
   )
 }
