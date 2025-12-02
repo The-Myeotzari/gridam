3e79056a7e1d4c975b0cb1fd5b571b7ec5eb0e76
cji <gugugeul273@gmail.com>
2025-12-03 02:36:06 +0900

    refactor:fetchCalendarMonth 추가

--- gridam/src/app/(main)/calendar/action.ts
+++ gridam/src/app/(main)/calendar/action.ts
@@ -11,6 +11,11 @@ type FetchCalendarParams = {
   day?: number
 }
 
+type fetchCalendarMonthParams = {
+  year?: number
+  month?: number
+}
+
 export type CalendarResponse = {
   ok: boolean
   data: {
@@ -37,7 +42,7 @@ export async function fetchCalendar(params: FetchCalendarParams = {}): Promise<C
   })
 
   const res = await fetch(
-    `${process.env.NEXT_PUBLIC_API_BASE_URL}/calendar?${searchParams.toString()}`,
+    `${process.env.NEXT_PUBLIC_API_BASE_URL}/calendar/day?${searchParams.toString()}`,
     {
       method: 'GET',
       credentials: 'include',
@@ -49,5 +54,32 @@ export async function fetchCalendar(params: FetchCalendarParams = {}): Promise<C
     }
   )
 
+  return res.json()
+}
+
+//특정 달 monthlyData (일기, 메모를 표시하기 위함.)
+export async function fetchCalendarMonth(params: fetchCalendarMonthParams = {}) {
+  const today = new Date()
+
+  const year = params.year ?? today.getFullYear()
+  const month = params.month ?? today.getMonth() + 1
+
+  const cookieHeader = await getCookies()
+
+  const searchParams = new URLSearchParams({
+    year: String(year),
+    month: String(month),
+  })
+
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_API_BASE_URL}/calendar/month?${searchParams.toString()}`,
+    {
+      method: 'GET',
+      credentials: 'include',
+      cache: 'no-cache',
+      next: { revalidate: 0 },
+      headers: { Cookie: cookieHeader },
+    }
+  )
   return res.json()
 }
