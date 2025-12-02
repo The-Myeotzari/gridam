bde53c79a8a9df55df600a6de79e8ecbdf44c759
cji <gugugeul273@gmail.com>
2025-12-02 15:43:25 +0900

    Rename:features/calendar로 폴더 변경 및 경로 수정

--- gridam/src/app/(main)/calendar/page.tsx
+++ gridam/src/app/(main)/calendar/page.tsx
@@ -1,5 +1,5 @@
 import { fetchCalendar } from '@/app/(main)/calendar/action'
-import CalendarClient from './components/calendar-client'
+import CalendarClient from '@/features/calendar/components/calendar-client'
 
 export default async function Page() {
   // 오늘 날짜 (전역 관리)
@@ -26,7 +26,7 @@ export default async function Page() {
         <h1 className="font-handwritten text-4xl mb-2 text-navy-gray font-bold">캘린더</h1>
         <p className="font-handwritten text-xl text-muted-foreground">일기와 메모를 한눈에</p>
       </div>
-      <CalendarClient initialDate={initialDate} initialData={initialData}></CalendarClient>
+      <CalendarClient initialDate={initialDate} initialData={initialData} />
     </div>
   )
 }
