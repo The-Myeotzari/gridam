3a41c25c8425c05249a3021d90aa05c37cbc0b19
OlMinJe <Ol.MinJe@gmail.com>
2025-12-03 15:58:12 +0900

    🚚 file: font 경로 public/font로 이동 #297

--- gridam/src/app/layout.tsx
+++ gridam/src/app/layout.tsx
@@ -6,16 +6,15 @@ import type { Metadata } from 'next'
 import localFont from 'next/font/local'
 
 const zenSerif = localFont({
-  src: '../shared/font/ZEN-SERIF-TTF-Regular.woff2',
+  src: '../../public/font/ZEN-SERIF-TTF-Regular.woff2',
   variable: '--font-zen-serif',
 })
 
 export const metadata: Metadata = {
   title: 'Gridam',
   description: '그리담 홈페이지',
-  keywords: ['그리담', 'Gridam', '그림일기'],  
+  keywords: ['그리담', 'Gridam', '그림일기'],
 }
-
 
 export default function RootLayout({
   children,
