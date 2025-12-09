8e827ab187457328ba87858839cc81bab867889e
OlMinJe <Ol.MinJe@gmail.com>
2025-12-09 14:24:00 +0900

    ♻️ refactor: 수정 페이지 진입시 canvas 초기값 할당 조건 이미지가 있을 떄로 변경 #350

--- gridam/src/shared/ui/canvas/canvas-container.tsx
+++ gridam/src/shared/ui/canvas/canvas-container.tsx
@@ -11,7 +11,7 @@ function CanvasContainer({ initialImage }: { initialImage?: string | null }) {
 
   useEffect(() => {
     if (initialImage) setImage(initialImage)
-  }, [initialImage, setImage])
+  }, [initialImage])
 
   const {
     canvasRef,
