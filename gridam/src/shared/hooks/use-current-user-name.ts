b4f82e543e89bd4dcda1982af7283f9a6ebac855
OlMinJe <Ol.MinJe@gmail.com>
2025-12-11 13:40:39 +0900

    ✨ feat: suapbase 유저 이름 가져오는 훅 구현(공식문서 참고) #335

---
+++ gridam/src/shared/hooks/use-current-user-name.ts
@@ -0,0 +1,23 @@
+'use clinet'
+
+import getSupabaseBrowserClient from '@/shared/utils/supabase/client'
+import { useEffect, useState } from 'react'
+
+export const useCurrentUserName = () => {
+  const [name, setName] = useState<string | null>(null)
+
+  useEffect(() => {
+    const fetchProfileName = async () => {
+      const { data, error } = await getSupabaseBrowserClient().auth.getSession()
+      if (error) {
+        console.error(error)
+      }
+
+      setName(data.session?.user.user_metadata.full_name ?? '?')
+    }
+
+    fetchProfileName()
+  }, [])
+
+  return name || '?'
+}
